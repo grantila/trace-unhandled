bfde536fa86056fa8e3c8bcf2b162b416f7cd6c0
Gustaf Räntilä <g.rantila@gmail.com>
2019-07-29 13:55:22 +0200

    fix(jest): Forced jest 20 (later interfere with unhandled exception warnings)
    Also improved code coverage

--- test/register.spec.js
+++ test/register.spec.js
@@ -105,4 +105,89 @@ describe( "register", ( ) =>
 		expect( linesWoLocation.length ).toBeGreaterThanOrEqual( 2 );
 		expect( linesWoLocation[ 0 ] ).toBe( linesWoLocation[ 1 ] );
 	} ) );
+
+	it( "Handle throwing in <new Promise>", withConsoleSpy( async ( ) =>
+	{
+		const err = new Error( "the error" );
+
+		new Promise( ( resolve, reject ) =>
+		{
+			throw err;
+		} );
+
+		await triggerUnhandledWarnings( );
+
+		const linesWoColumns = splitLines( console.error.mock.calls[ 0 ] )
+			.filter( line => line.includes( "register.spec.js" ) )
+			.map( line => cutColumn( line ) );
+
+		const linesWoLocation = splitLines( console.error.mock.calls[ 0 ] )
+			.filter( line => line.includes( "register.spec.js" ) )
+			.map( line => cutLocation( line ) );
+
+		expect( linesWoColumns.length ).toBeGreaterThanOrEqual( 2 );
+		expect( linesWoColumns[ 0 ] ).not.toBe( linesWoColumns[ 1 ] );
+
+		expect( linesWoLocation.length ).toBeGreaterThanOrEqual( 2 );
+		expect( linesWoLocation[ 0 ] ).toBe( linesWoLocation[ 1 ] );
+	} ) );
+
+	it( "Handle error without stack", withConsoleSpy( async ( ) =>
+	{
+		const err = new Error( "the error" );
+		delete err.stack;
+
+		new Promise( ( resolve, reject ) =>
+		{
+			throw err;
+		} );
+
+		await triggerUnhandledWarnings( );
+
+		const linesWoColumns = splitLines( console.error.mock.calls[ 0 ] )
+			.filter( line => line.includes( "register.spec.js" ) )
+			.map( line => cutColumn( line ) );
+
+		const errorAndShared = splitLines( console.error.mock.calls[ 0 ] )
+			.filter( line => line );
+
+		expect( linesWoColumns.length ).toBeGreaterThanOrEqual( 2 );
+		expect( linesWoColumns[ 0 ] ).not.toBe( linesWoColumns[ 1 ] );
+
+		expect( errorAndShared[ errorAndShared.length - 2 ] ).toBe(
+			"    ==== Error at: ===================="
+		);
+		expect( errorAndShared[ errorAndShared.length - 1 ] ).toBe(
+			"    ==== Shared trace: ================"
+		);
+	} ) );
+
+	it( "Handle null error", withConsoleSpy( async ( ) =>
+	{
+		const err = null;
+
+		new Promise( ( resolve, reject ) =>
+		{
+			throw err;
+		} );
+
+		await triggerUnhandledWarnings( );
+
+		const linesWoColumns = splitLines( console.error.mock.calls[ 0 ] )
+			.filter( line => line.includes( "register.spec.js" ) )
+			.map( line => cutColumn( line ) );
+
+		const errorAndShared = splitLines( console.error.mock.calls[ 0 ] )
+			.filter( line => line );
+
+		expect( linesWoColumns.length ).toBeGreaterThanOrEqual( 2 );
+		expect( linesWoColumns[ 0 ] ).not.toBe( linesWoColumns[ 1 ] );
+
+		expect( errorAndShared[ errorAndShared.length - 2 ] ).toBe(
+			"    ==== Error at: ===================="
+		);
+		expect( errorAndShared[ errorAndShared.length - 1 ] ).toBe(
+			"    ==== Shared trace: ================"
+		);
+	} ) );
 } );
