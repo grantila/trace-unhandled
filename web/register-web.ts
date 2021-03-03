c5f556d65b96cb6122c41b997a4e3f5eb20a59b1
Gustaf Räntilä <g.rantila@gmail.com>
2021-03-03 19:10:28 +0100

    fix(web): added missing typing

--- web/register-web.ts
+++ web/register-web.ts
@@ -1,7 +1,7 @@
 
 import { logger, TraceablePromise } from "../lib/core";
 
-window.onunhandledrejection = function( event )
+window.onunhandledrejection = function( event: PromiseRejectionEvent )
 {
 	logger( event.reason, event.promise );
 };
